a4f2688672ff28263e1a3f1dbb5d6d5cb11c508a
salvatoreromeo <info@salvatoreromeo.com>
2022-07-05 10:23:45 +0200

    Add button group

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{scss}", "./index.html"],
-  important: true,
   mode: "jit",
   theme: {
     extend: {
-        colors: {
-          "bop": "var(--bo-primary-color)",
-          "bos": "var(--bo-secondary-color)"
-        }
+      colors: {
+        bop: "var(--bo-primary-color)",
+        bos: "var(--bo-secondary-color)",
+      },
     },
   },
   plugins: [],
-}
+};
